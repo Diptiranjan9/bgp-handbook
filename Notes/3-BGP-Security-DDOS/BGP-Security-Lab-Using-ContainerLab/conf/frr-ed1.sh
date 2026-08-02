904c6410ae1ef8e0ddc3577adb6769e912b38237
Diptiranjan Sahoo <sahoo.dipti.9937@gmail.com>
2026-08-02 12:55:55 +0530

    change in file name

--- Notes/3-BGP-Security-DDOS/BGP-Security-Lab-Using-ContainerLab/conf/frr-ed1.sh
+++ Notes/3-BGP-Security-DDOS/BGP-Security-Lab-Using-ContainerLab/conf/frr-ed1.sh
@@ -4,11 +4,11 @@ ip link set dev eth2 mtu 1500
 ip link set dev eth3 mtu 1500
 ip link set dev eth4 mtu 1500
 ip link set dev eth5 mtu 1500
-#chmod +x /usr/local/bin/sync_flowspec_ipt.py
-#/usr/local/bin/sync_flowspec_ipt.py >/var/log/sync_flowspec_ipt.log 2>&1 &
-chmod +x /usr/local/bin/frr_universal_flowspec.py
-/usr/local/bin/frr_universal_flowspec.py >/var/log/frr_universal_flowspec.log 2>&1 &
+chmod +x /usr/local/bin/frr-flowspec-to-iptable.py
+/usr/local/bin/frr-flowspec-to-iptable.py >/var/log/frr-flowspec-to-iptable.log 2>&1 &
 sysctl -p /etc/sysctl.d/99-rpf.conf
+
+#verification commands
 #sysctl -a | grep rp_filter
 #verification command in frr bash for ip table
 #ipset list frr_ip_set
